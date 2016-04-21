371dced526eebf79b1d95f51fd588b2d39348ba6
ronathan <ronaldvictorino@gmail.com>
2016-04-20 20:52:31 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-main.js
+$(function(){
+
+	var results;
+
+	jQuery.ajax({
+	  url: 'https://lcboapi.com/stores',
+	  headers: { 'Authorization': 'Token MDpkZDhkYmU0Mi0wNmFhLTExZTYtYjRlMS01YjE1ZjY2OWE2NWE6RjFldVVNQ2pVS0daeUw3SjJmNHlkVmRhazlrc3NpRlUxOXVu' },
+	  dataType: 'json'
+	}).then(function(data) {
+		
+		console.log(data);
+		stores = data.result;
+
+		for(var i = 0; i < stores.length; i++) {
+			console.log(stores[i].name);
+			$('ul#storelist').append('<li>' + stores[i].name + '</li>');
+		}
+		
+	});
+});
