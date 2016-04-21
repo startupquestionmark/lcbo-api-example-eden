bc7f416c6078c313733482ecb02e8fbca2ce92f9
ronathan <ronaldvictorino@gmail.com>
2016-04-20 21:28:25 -0400

    added images and prodcts list

--- js/main.js
+++ js/main.js
@@ -17,4 +17,25 @@ $(function(){
 		}
 		
 	});
+
+	jQuery.ajax({
+  		url: 'https://lcboapi.com/products?q=Armand+de+Brignac',
+  		headers: { 'Authorization': 'Token MDpkZDhkYmU0Mi0wNmFhLTExZTYtYjRlMS01YjE1ZjY2OWE2NWE6RjFldVVNQ2pVS0daeUw3SjJmNHlkVmRhazlrc3NpRlUxOXVu' },
+  		dataType: 'json'
+	}).then(function(data) {
+  
+  	console.log(data);
+  	beers = data.result;
+
+  	for(var i=0; i<beers.length;i++){
+  		console.log(beers[i].producer_name);
+  		console.log(beers[i].image_thumb_url);
+  		$('ul#beer').append('<li>' + '<img src="' + beers[i].image_thumb_url + '"/>'+ beers[i].producer_name + '</li>');
+
+  	}
+
+
+});
+
+
 });
